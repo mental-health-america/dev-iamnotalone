d238f803072c1e145452c1e19affdc0dbcb181d0
Mohamed Hassan <mhassan@mhanational.org>
2021-09-23 18:16:12 -0400

    OpenGraph & edits

--- iamnotalone/resources/views/create_event.blade.php
+++ iamnotalone/resources/views/create_event.blade.php
@@ -36,11 +36,12 @@
                                     <option value="Training">Training</label>
                                     <option value="Concert">Concert</label>
                                     <option value="Activities">Activity</label>
+                                    <option value="Other">Other</label>
                                 </select>
                             </div>
 
                             <div class="py-2 text-left">
-                                <label for="event_name" class="block text-xs mb-2">Activity Banner (PNG, JPG; 750 * 250 px)</label>
+                                <label for="event_name" class="block text-xs mb-2">Activity Flyer (PNG, JPG; 750 * 250 px) <b>MUST HAVE THE RECOMMENDED SPECIFICATIONS</b></label>
                                 <input type="file" class="border text-sm border-gray-100 focus:outline-none block w-full p-4 rounded-lg focus:border-gray-700" accept="image/*" name="banner" placeholder="Upload Image" value="{{old('banner')}}"/>
                             </div>
                         </div>
