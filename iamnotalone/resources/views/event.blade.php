c8e4628a27f71968c614256db74cfccc35f56ae5
Georgia Tech <georgiatech@ipsec-10-2-96-90.vpn.gatech.edu>
2022-12-08 14:02:39 -0500

    restore registar btn

--- iamnotalone/resources/views/event.blade.php
+++ iamnotalone/resources/views/event.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.master')
 @section('css')
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>    
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 @endsection
 @section('content')
     <div class="container">
@@ -26,16 +26,16 @@
                     <p class="text-sm text-gray-600 my-2 font-semibold"> <span class="uil uil-map-marker text-lg"></span> {{$event->venue.', '.$event->address1.'. '. $event->city.', '.$event->state}} </p>
                 @endif
                 @if($event->online)
-                    <p class="text-sm text-gray-600 my-4 font-semibold"> {{$event->platform}} <span class="uil uil-link text-lg"></span> 
+                    <p class="text-sm text-gray-600 my-4 font-semibold"> {{$event->platform}} <span class="uil uil-link text-lg"></span>
                     @if($event->link)
-                    <a href="{{$event->link}}">{{$event->link}}</a> 
+                    <a href="{{$event->link}}">{{$event->link}}</a>
                     @endif
                     @if($event->registration_link)
                     <a href="{{$event->registration_link}}">{{$event->registration_link}}</a>
                     @endif
                     </p>
                 @endif
-               
+
                 <p class="text-gray-600 mt-4 text-sm">Disability Accomodations</p>
                 @if (count($accomodations))
                     <ul class="pl-8 list-disc inline-block text-sm">
@@ -47,15 +47,15 @@
                     <ul class="pl-8 list-disc inline-block text-sm">
                         <li>None</li>
                     </ul>
-                @endif  
-               
+                @endif
+
                 <p class="text-sm text-yellow-500 my-4 font-semibold"> <span class="uil uil-clock text-lg text-black"></span> {{\Carbon\Carbon::parse($event->start_date)->format('l')}}, {{date('h:i a', strtotime($event->start_time))}} EST - {{\Carbon\Carbon::parse($event->end_date)->format('l')}}, {{date('h:i a', strtotime($event->end_time))}} EST</p>
 
-                <!-- <p class="mt-12">
+               <p class="mt-12">
                     <a href="#" id="register" class="py-3 px-12 bg-primary hover:bg-indigo-700 focus:ring-indigo-500 flex-nowrap focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         Register <i class="uil uil-check"></i>
                     </a>
-                </p> -->
+                </p>
             </div>
         </div>
     </div>
